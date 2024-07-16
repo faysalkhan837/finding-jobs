e9833c813e730a4cab986063767cd76e6ebf1679
faysalkhan837 <faysal.khan837@gmail.com>
2024-07-17 00:56:54 +0600

    new commit

--- src/Component/MyPostedJobBox/MyPostedJobBox.jsx
+++ src/Component/MyPostedJobBox/MyPostedJobBox.jsx
@@ -1,7 +1,35 @@
+import axios from "axios";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyPostedJobBox = ({ item }) => {
-  console.log(item._id)
+
+  const handleDeletJob = () => {
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You wont to delet this?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete(`http://localhost:5000/jobData/${item?._id}`)
+          .then(res => {
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          })
+      }
+    });
+  }
+
   return (
     <div className="w-64 p-4 m-auto bg-white shadow-lg rounded-2xl shadow-black">
       <div className="w-full h-full">
@@ -23,16 +51,23 @@ const MyPostedJobBox = ({ item }) => {
             <b>Description:</b> {item?.short_details}
           </p>
         </div>
-       <Link to={`/editJob/${item._id}`}>
-       <div className="flex items-center justify-between w-full gap-4">
-            <button
+        <div className="flex gap-x-2">
+        <Link className="w-full" to={`/editJob/${item._id}`}>
+        <button
               type="button"
               className="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
             >
               Edit
             </button>
+        </Link>
+         <button
+              onClick={handleDeletJob}
+              type="button"
+              className="py-2 px-4 bg-red-500 hover:bg-red-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
+            >
+              Delet
+            </button>
         </div>
-       </Link>
       </div>
     </div>
   );
